56ccf83442aaff5484088f2210053fe3a233a2b6
Szymon Standarski <s.standarski@gmail.com>
2019-04-01 09:14:25 +0200

    feat(logger): dodano obsługę log4js logi idą na stdout oraz do pliku shoper.log

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
+import { configure, getLogger } from "log4js";
+
+configure({
+  appenders: {
+    out: { type: "stdout" },
+    app: { type: "file", filename: "log4js/shoper.log", maxLogSize: 10485760, numBackups: 3 }
+  },
+  categories: {
+    default: { appenders: ["out", "app"], level: "trace" }
+  }
+});
+
+const logger = getLogger();
+
 export function init() {
+  logger.trace("Start serwisu shoperintergration");
 }
 
 init();
