2dce512bb24cc4c3e15a66067fcd286716fbb164
Szymon Standarski <s.standarski@gmail.com>
2019-04-04 08:42:25 +0200

    test(index): zmodyfikowano testy związane z przeniesieniem konfiguracji do osobnej klasy

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,16 +1,38 @@
 // import * as index from "./index";
-import { init } from "./index";
+// import { init, config, logger, loadDefaultConfiguration, CONFIG_FILE_NAME } from "./index";
+var Index;
 
-describe("Index init", () => {
-  test("Czy index zawiera funkcję inicjującą ", done => {
-    expect(init).toBeDefined();
-    done()
+describe("Index", () => {
+  beforeAll(() => {
+    Index = undefined;
+    Index = require("./index");
+  });
+
+  test("powinien zawierać funkcję inicjującą", done => {
+    expect(Index.init).toBeDefined();
+    done();
+  });
+
+  test("powinien zawierać instancję loggera", done => {
+    Index.init();
+    expect(Index.logger).toBeDefined();
+    done();
+  });
+
+  test("index.ts powinien zawierać załadowaną konfigurację z pliku config.json", done => {
+    expect(Index.config).toBeDefined();
+    done();
+  });
+
+  test("w konfiguracji powinien znajdować się obiekt log4js z konfiguracją dla loggiera", done => {
+    expect(Index.config.log4js).toBeDefined();
+    done();
+  });
+
+  test("typ konfiguracji powinien wskazywać na plik", done => {
+    expect(Index.config.configurationType).toBeDefined();
+    expect(Index.config.configurationType).toContain(Index.CONFIG_FILE_NAME);
+    done();
   });
 });
 
-// describe("Index logger", () => {
-//   test("Czy istnieje logger", done => {
-//     expect(index.logger).toBeDefined();
-//     done()
-//   });
-// });
