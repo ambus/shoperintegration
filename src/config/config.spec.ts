f8e4e3c8eaeeb371a0a5c26e6426e1de4fe5bb83
Szymon Standarski <s.standarski@gmail.com>
2019-04-04 08:40:07 +0200

    refactor(config): przeniesiono konfigurację do zewnętrznej klasy

---
+++ src/config/config.spec.ts
@@ -0,0 +1,50 @@
+import { Config } from "./config";
+import * as fs from "fs";
+
+const TEST_CONFIG_FILE_PATH = "config.json";
+
+describe("Konfiguracja", () => {
+  var config: Config;
+  beforeAll(() => {
+    config = Config.getInstance();
+  });
+
+  test("w konfiguracji powinien znajdować się obiekt log4js z konfiguracją dla loggiera", done => {
+    expect(config.log4js).toBeDefined();
+    done();
+  });
+
+  test("typ konfiguracji powinien wskazywać na plik", done => {
+    expect(config.configurationType).toBeDefined();
+    expect(config.configurationType).toContain(TEST_CONFIG_FILE_PATH);
+    done();
+  });
+
+  test("każda kolejne utworzenie obiektu powinno być tą samą instancję", done => {
+    const newInfo = "New info data";
+    expect(config.configurationType).not.toEqual(newInfo);
+    config.configurationType = newInfo;
+    let configV2 = Config.getInstance();
+    expect(configV2.configurationType).toEqual(newInfo);
+    done();
+  });
+});
+
+describe("Konfiguracja - domyślne wartości", () => {
+  var config: Config;
+
+  beforeAll(() => {
+    fs.renameSync(TEST_CONFIG_FILE_PATH, `test${TEST_CONFIG_FILE_PATH}`);
+    config = Config.getInstance(TEST_CONFIG_FILE_PATH + "fff");
+  });
+
+  test("jeśli plik z konfiguracją nie istnieje lub zawiera błędy powinna zostać załadowana domyślna konfiguracja", done => {
+    expect(config).toBeDefined();
+    expect(config.log4js).toBeDefined();
+    expect(config.configurationType).toEqual("default");
+    done();
+  });
+  afterAll(() => {
+    fs.renameSync(`test${TEST_CONFIG_FILE_PATH}`, TEST_CONFIG_FILE_PATH);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,14 @@
-import { configure, getLogger } from "log4js";
-import * as fs from "fs";
-import { Config } from "./models/config";
+import { configure, getLogger, Logger } from "log4js";
+import { Config } from "./config/config";
 
-const CONFIG_FILE_NAME = "config.json";
-var config: Config;
-
-try {
-  config = JSON.parse(fs.readFileSync(CONFIG_FILE_NAME, "utf8"));
-} catch (err) {
-  console.error(`Napotkano błąd podczas pobierania konfiguracji. Próbowano odnaleść plik konfiguracyjny pod adresem ${CONFIG_FILE_NAME}. Ustawiono domyślną konfigurację`, err);
-  config = {
-    log4js: {
-      appenders: {
-        out: { type: "stdout" }
-      },
-      categories: {
-        default: { appenders: ["out"], level: "trace" }
-      }
-    }
-  };
-}
-
-configure(config.log4js);
-
-const logger = getLogger();
+export const CONFIG_FILE_NAME = "config.json";
+export var config: Config;
+export var logger: Logger;
 
 export function init() {
+  config = Config.getInstance(CONFIG_FILE_NAME);
+  configure(config.log4js);
+  logger = getLogger();
   logger.trace("Start serwisu shoperintergration");
 }
 
