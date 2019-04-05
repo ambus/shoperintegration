a24cc3548614cf7bb0c83b285ff9788964517e51
Szymon Standarski <s.standarski@gmail.com>
2019-04-05 09:12:31 +0200

    test(index): zmieniono test na it

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -8,28 +8,28 @@ describe("Index", () => {
     Index = require("./index");
   });
 
-  test("powinien zawierać funkcję inicjującą", done => {
+  it("powinien zawierać funkcję inicjującą", done => {
     expect(Index.init).toBeDefined();
     done();
   });
 
-  test("powinien zawierać instancję loggera", done => {
+  it("powinien zawierać instancję loggera", done => {
     Index.init();
     expect(Index.logger).toBeDefined();
     done();
   });
 
-  test("index.ts powinien zawierać załadowaną konfigurację z pliku config.json", done => {
+  it("index.ts powinien zawierać załadowaną konfigurację z pliku config.json", done => {
     expect(Index.config).toBeDefined();
     done();
   });
 
-  test("w konfiguracji powinien znajdować się obiekt log4js z konfiguracją dla loggiera", done => {
+  it("w konfiguracji powinien znajdować się obiekt log4js z konfiguracją dla loggiera", done => {
     expect(Index.config.log4js).toBeDefined();
     done();
   });
 
-  test("typ konfiguracji powinien wskazywać na plik", done => {
+  it("typ konfiguracji powinien wskazywać na plik", done => {
     expect(Index.config.configurationType).toBeDefined();
     expect(Index.config.configurationType).toContain(Index.CONFIG_FILE_NAME);
     done();
