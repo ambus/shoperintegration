6f53374c3b5ec81faf5e6d7ec2de0b2a96447f35
Szymon Standarski <s.standarski@gmail.com>
2019-04-05 09:11:57 +0200

    test(fileWatcher): dodano podstawowe testy dla FileWatcher'a

--- src/file-watcher/file-watcher.spec.ts
+++ src/file-watcher/file-watcher.spec.ts
@@ -1,19 +1,43 @@
-import { FileWacher } from "./file-watcher";
+import { FileWatcher } from "./file-watcher";
+const path = require("path");
+const TEST_FILE_PATH = "../../tmp/test.csv";
 
 describe("FileWatcher", () => {
-  test("Czy jest zdefiniowana klasa FileWatcher", done => {
-    expect(FileWacher).toBeDefined();
-    done()
+  let fileWatcher: FileWatcher;
+  beforeEach(() => {
+    fileWatcher = new FileWatcher();
+  })
+
+  it("Czy jest zdefiniowana klasa FileWatcher", done => {
+    expect(FileWatcher).toBeDefined();
+    done();
   });
 
-  test("powinien istnieć strumień z przychodzącymi danymi - do którego można się podpiąć", done => {
+  it("powinien zawierać instancję loggera", () => {
+    expect(fileWatcher.logger).toBeDefined();
   });
-  test("w strumieniu powinny pojawić się dane wgrane do obserwowanego pliku", done => {
+
+  it("powinien zawierać funkcję uruchamiającą obserwowanie plików", () => {
+    expect(fileWatcher.startWatch).toBeDefined();
   });
-  test("powinien usunąć plik z danymi po przeczytaniu ich", done => {
+
+  it("można pobrać informację czy plik jest obserwowany", () => {
+    expect(fileWatcher.isWatching).toBeFalsy();
+    fileWatcher.startWatch(path.resolve(__dirname, TEST_FILE_PATH), true);
+    expect(fileWatcher.isWatching).toBeTruthy();
   });
-  test("klasa powinna prawidłowo odczytać wgrane dane do pliku", done => {
+
+  it("w przypadku napotkania błędów podczas odczytu startWatch powinien wyrzucić wyjątek", () => {
+    fileWatcher.startWatch(path.resolve(__dirname, `${TEST_FILE_PATH}blad`), true);
+  })
+
+  it("powinien istnieć strumień z przychodzącymi danymi - do którego można się podpiąć", () => {
+    expect(fileWatcher.streamWithDataInsertedToWatchingFile).toBeDefined();
   });
-  test("w przypadku błędu odczytu dane powinny zostać zalogowane ", done => {
-  });
+
+  it("w strumieniu powinny pojawić się dane wgrane do obserwowanego pliku", () => {});
+  it("po zmianie danych w pliku powinny pojawić się dane w strumieniu", () => {});
+  it("powinien usunąć plik z danymi po przeczytaniu ich", () => {});
+  it("klasa powinna prawidłowo odczytać wgrane dane do pliku", () => {});
+  it("w przypadku błędu odczytu dane powinny zostać zalogowane ", () => {});
 });
