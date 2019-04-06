386bd93573bf373eda8889525f834fcaeb1ede0d
Szymon Standarski <s.standarski@gmail.com>
2019-04-06 07:11:23 +0200

    feat(filewatcher): dodano funkcję odczytywania pliku i przekazywania funkcji do strumienia

--- src/file-watcher/file-watcher.ts
+++ src/file-watcher/file-watcher.ts
@@ -23,9 +23,9 @@ export class FileWatcher {
     this.logger.debug(`Start obserwowania pliku ${filepath}`);
     try {
       if (readOnStart) {
-        // this.readFileAndSendThemToStream(`${filepath}`);
+        this.readFileAndSendThemToStream(`${filepath}`);
       }
-      //   this.watchFile(filepath);
+      this.watchFile(filepath);
       this.watchingFile = filepath;
     } catch (err) {
       this.logger.error(`Napotkano błąd podczas odczytu pliku ${filepath}. Wymagane jest ponowne uruchomienie obserwowania plików!`, err);
@@ -36,5 +36,37 @@ export class FileWatcher {
 
   public get isWatching(): boolean {
     return !!(this.watchingFile && this.watchingFile.length > 0);
+  }
+
+  public readFileAndSendThemToStream(filePath: string): Promise<void> {
+    this.logger.debug(`Próba odczytania pliku ${filePath}`);
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (fs.existsSync(filePath)) {
+          let returnString = await this.readFile(filePath);
+          await this.deleteFile(filePath);
+          this.$dataFromFileStream.next(returnString);
+        } else {
+          this.logger.debug(`Plik ${filePath} nie istnieje`);
+        }
+        resolve();
+      } catch (err) {
+        this.logger.error(`Napotkano błąd podczas próby odczytu pliku ${filePath}`, err);
+        throw err;
+      }
+    });
+  }
+
+  public readFile(filePath: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, this.config.encoding, (err: Error, data: string) => {
+        if (err) {
+          this.logger.error(`Napotkano błąd podczas próby odczytu pliku ${filePath}:`, err);
+          throw err;
+        }
+        this.logger.info(`Odczytano nowe dane:`, data);
+        resolve(data);
+      });
+    });
   }
 }
