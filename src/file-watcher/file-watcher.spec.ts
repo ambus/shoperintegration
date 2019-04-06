79eee541300b0dea17e9636573f5db6daba485f6
Szymon Standarski <s.standarski@gmail.com>
2019-04-06 07:03:45 +0200

    w teście dodano przykładowe dane umieszczane w pliku do obserwowania

--- src/file-watcher/file-watcher.spec.ts
+++ src/file-watcher/file-watcher.spec.ts
@@ -1,6 +1,12 @@
 import { FileWatcher } from "./file-watcher";
 const path = require("path");
 const TEST_FILE_PATH = "../../tmp/test.csv";
+const EXAMPLE_DATA = `product_code;stock;price
+BUKWT2010;  41;110,0
+BSZK0F1FLE051;   3;139,57
+BSZK0F1FLE011;   3;139,57
+BSZK0IZMWAS02;   2;16,00
+ICAZG10KLD;  42;120,0`;
 
 describe("FileWatcher", () => {
   let fileWatcher: FileWatcher;
