3732c596c966945e61232b1449f2f57ee843f3b4
Szymon Standarski <s.standarski@gmail.com>
2019-04-14 06:38:50 +0200

    feat(csv-parser): dodano funkcję parsującą obiekt csv do javascript object

---
+++ src/csv-parser/csv-parser.ts
@@ -0,0 +1,41 @@
+import { getLogger } from "log4js";
+import { FilonMerchandise } from "../models/filon-merchandise";
+import { Config } from "../config/config";
+import parse = require("csv-parse/lib/sync");
+
+const PARSE_ERROR = "Napotkano błąd podczas parsowania otrzymanych danych! Prawdopodobnie jest błąd w strukturze stringa";
+
+const logger = getLogger();
+
+this.logger = getLogger();
+export function parseCSVDataToFilonMerchandise(dataToParse: string, config: Config): FilonMerchandise[] {
+  let parsedMerchandiseArray: FilonMerchandise[] = [];
+  logger.debug("Dane do parsowania: ", dataToParse);
+  try {
+    parsedMerchandiseArray = replaceCommaInFilonMerchandisesPrices(parse(dataToParse, config.parserOptions));
+
+    if (firstoObjectHavaAlleRequiredField(parsedMerchandiseArray)) {
+      logger.error("Dane przekazane do parsowania niestety nie posiadają wymaganych pól! Zwrócono pustą tablicę.");
+      return [];
+    }
+  } catch (err) {
+    logger.error("Podczas parsowania danych napotkano błąd", err);
+    throw new Error(PARSE_ERROR);
+  }
+  logger.debug(`Parsowanie zakończyło się powodzeniem. Odczytano ${parsedMerchandiseArray.length} towarów. Dane po parsowaniu:`, parsedMerchandiseArray);
+  return parsedMerchandiseArray;
+}
+
+function firstoObjectHavaAlleRequiredField(merchandise: FilonMerchandise[]): boolean {
+  return merchandise.length > 0 && (typeof merchandise[0].product_code === "undefined" || typeof merchandise[0].price === "undefined" || typeof merchandise[0].stock === "undefined");
+}
+
+function replaceCommaInFilonMerchandisesPrices(merchandise: FilonMerchandise[]): FilonMerchandise[] {
+  merchandise.map((merchandise: FilonMerchandise) => {
+    merchandise.price = merchandise.price.replace(",", ".");
+    merchandise.priceE = merchandise.priceE.replace(",", ".");
+  });
+  logger.debug(`Dane po zamienie przecinków na kropki:`, merchandise);
+
+  return merchandise;
+}
