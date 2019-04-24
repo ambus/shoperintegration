97f4633dcbf29e018c2cc76c22d24d44ce92bb10
Szymon Standarski <s.standarski@gmail.com>
2019-04-24 22:25:06 +0200

    refactor(shoperGetToken): usunięcie niepotrzebnych komentarzy i funkcji

--- src/shoper/shoper-get-token.ts
+++ src/shoper/shoper-get-token.ts
@@ -1,25 +1,17 @@
-import { Observable, iif, throwError, of } from "rxjs";
+import { Observable } from "rxjs";
 import { ajax, AjaxResponse } from "rxjs/ajax";
 import { AnonymousSubject } from "rxjs/internal/Subject";
 import { Config } from "../config/config";
-import { retryWhen, concatMap, tap, delay, map } from "rxjs/operators";
+import { tap, map } from "rxjs/operators";
 import { XMLHttpRequest } from "xmlhttprequest";
 
 export class ShoperGetToken {
   public static authorizationToken: string;
 
-  // public static retryPipeline = retryWhen(errors =>
-  //   errors.pipe(concatMap((e, i) => iif(() => i > Config.getInstance().attempsWhenError, throwError(e), of(e).pipe(delay(Config.getInstance().errorDelayTime)))))
-  // );
-
-  static createXHR = function(){
-    return new XMLHttpRequest();
-  }
-
-  public static getToken(userToken: string, refreshToken: boolean = true, createXHR: XMLHttpRequest = ShoperGetToken.createXHR): Observable<string> {
+  public static getToken(userToken: string, refreshToken: boolean): Observable<string> {
+    let createXHR = new XMLHttpRequest()
     if (refreshToken || !this.authorizationToken) {
       return ajax({ createXHR, url: Config.getInstance().shoperConfig.urls.token, crossDomain: true, withCredentials: false, method: "POST", headers: { Authorization: `Basic ${userToken}` } }).pipe(
-        tap((token: AjaxResponse) => console.warn(token.response)),
         map((token: AjaxResponse) => token.response.access_token),
         tap((token: string) => (this.authorizationToken = token))
       );
