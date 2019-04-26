4b9aca93c0fda82e876b6627ac265eb7abeb7f00
Szymon Standarski <s.standarski@gmail.com>
2019-04-26 09:39:49 +0200

    feat(shoperService): po dodaniu towaru do serwisu jest on konwertowany do Taska i wrzucany do strumienia

--- src/shoper/shoper-service.spec.ts
+++ src/shoper/shoper-service.spec.ts
@@ -1,8 +1,38 @@
 import { Config } from "../config/config";
 import { ShoperService } from "./shoper-service";
+import { FilonMerchandise } from "../models/filon-merchandise";
+import { stringGenerator } from "../lib/string-generator";
+import { Task } from "../models/task";
 
 describe("shoperService", () => {
-    it("można utworzyć obiekt shoperService", () => {
-        expect(new ShoperService(Config.getInstance())).toBeDefined();
-    })
-})
+  it("można utworzyć obiekt shoperService", () => {
+    expect(new ShoperService(Config.getInstance())).toBeDefined();
+  });
+
+  it("można wywołać funkcję dodającą kolejne zadanie do kolejki", () => {
+    let shoperService = new ShoperService(Config.getInstance());
+    expect(shoperService.addTask).toBeDefined();
+  });
+
+  it("po dodaniu taska serwis powinien go przekazać do strumienia z zadaniami", done => {
+    let shoperService = new ShoperService(Config.getInstance());
+    let filonMerchandise: FilonMerchandise = { product_code: stringGenerator(), stock: 1, price: "16.00" };
+    shoperService._taskStock$.subscribe((val: Task) => {
+      expect(val.filonMerchandise.product_code).toEqual(filonMerchandise.product_code);
+      expect(val.id).toBeDefined();
+      done();
+    });
+    shoperService.addTask(filonMerchandise);
+  });
+
+  it("każde zadanie powinno posiadać swoje id", done => {
+    let shoperService = new ShoperService(Config.getInstance());
+    let filonMerchandise: FilonMerchandise = { product_code: stringGenerator(), stock: 1, price: "16.00" };
+    shoperService._taskStock$.subscribe((val: Task) => {
+      expect(val.id).toBeDefined();
+      expect(val.id.length).toBeGreaterThan(5);
+      done();
+    });
+    shoperService.addTask(filonMerchandise);
+  });
+});
--- src/shoper/shoper-service.ts
+++ src/shoper/shoper-service.ts
@@ -1,7 +1,22 @@
 import { Config } from "../config/config";
+import { FilonMerchandise } from "../models/filon-merchandise";
+import { Task } from "../models/task";
+import { Subject, Observable } from "rxjs";
+import { map, share } from "rxjs/operators";
+import { createTaskRequest } from "./utils/create-task-request";
 
 export class ShoperService {
-    constructor(conifg: Config) {
+    filonMerchandiseAdd$: Subject<FilonMerchandise> = new Subject();
 
+    _taskStock$: Observable<Task> = this.filonMerchandiseAdd$.pipe(
+        map(createTaskRequest),
+        share()
+    )
+
+    constructor(private conifg: Config) {
+    }
+
+    addTask(filonMerchandise: FilonMerchandise): void {
+        this.filonMerchandiseAdd$.next(filonMerchandise);
     }
 }
