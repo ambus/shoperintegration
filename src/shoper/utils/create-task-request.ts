671e77615d762148eddeafdc873b50a08fdc513e
Szymon Standarski <s.standarski@gmail.com>
2019-04-28 07:08:03 +0200

    feat(task): przy tworzeniu taska otrzymuje ono status requested

--- src/shoper/utils/create-task-request.ts
+++ src/shoper/utils/create-task-request.ts
@@ -1,10 +1,12 @@
 import { FilonMerchandise } from "../../models/filon-merchandise";
 import { Task } from "../../models/task";
 import { stringGenerator } from "../../lib/string-generator";
+import { TaskShoperRequestStatusValue } from "../../models/task-shoper-request-status-value";
 
 export const createTaskRequest: (filonMerchandise: FilonMerchandise) => Task = filonMerchandise => ({
     id: stringGenerator(),
     filonMerchandise: filonMerchandise,
     attemptCounter: 0,
-    lastAttemptTime: undefined
+    lastAttemptTime: undefined,
+    status: TaskShoperRequestStatusValue.requested
 });
