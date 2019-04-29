acc13fac40b5b2cc461a50bb329380f529b266ca
Szymon Standarski <s.standarski@gmail.com>
2019-04-29 07:00:48 +0200

    test(shoperService): po dodaniu zadania powinno ono uzyskać status making

--- src/shoper/shoper-service.spec.ts
+++ src/shoper/shoper-service.spec.ts
@@ -3,6 +3,7 @@ import { ShoperService } from "./shoper-service";
 import { FilonMerchandise } from "../models/filon-merchandise";
 import { stringGenerator } from "../lib/string-generator";
 import { Task } from "../models/task";
+import { TaskShoperRequestStatusValue } from "../models/task-shoper-request-status-value";
 
 describe("shoperService", () => {
   it("można utworzyć obiekt shoperService", () => {
@@ -17,7 +18,7 @@ describe("shoperService", () => {
   it("po dodaniu taska serwis powinien go przekazać do strumienia z zadaniami", done => {
     let shoperService = new ShoperService(Config.getInstance());
     let filonMerchandise: FilonMerchandise = { product_code: stringGenerator(), stock: 1, price: "16.00" };
-    shoperService._taskStock$.subscribe((val: Task) => {
+    shoperService._taskRequest$.subscribe((val: Task) => {
       expect(val.filonMerchandise.product_code).toEqual(filonMerchandise.product_code);
       expect(val.id).toBeDefined();
       done();
@@ -25,12 +26,25 @@ describe("shoperService", () => {
     shoperService.addTask(filonMerchandise);
   });
 
-  it("każde zadanie powinno posiadać swoje id", done => {
+  it("każde zadanie powinno posiadać swoje id i status requested", done => {
     let shoperService = new ShoperService(Config.getInstance());
     let filonMerchandise: FilonMerchandise = { product_code: stringGenerator(), stock: 1, price: "16.00" };
-    shoperService._taskStock$.subscribe((val: Task) => {
+    shoperService._taskRequest$.subscribe((val: Task) => {
       expect(val.id).toBeDefined();
       expect(val.id.length).toBeGreaterThan(5);
+      expect(val.status).toEqual(TaskShoperRequestStatusValue.requested);
+      done();
+    });
+    shoperService.addTask(filonMerchandise);
+  });
+
+  it("każde zadanie po dodaniu i zwolnieniu kolejki powinno się pojawic w strumieniu wykonywanych zadań", done => {
+    let shoperService = new ShoperService(Config.getInstance());
+    let filonMerchandise: FilonMerchandise = { product_code: stringGenerator(), stock: 1, price: "16.00" };
+    shoperService.makingTask$.subscribe((val: Task) => {
+      expect(val.id).toBeDefined();
+      expect(val.id.length).toBeGreaterThan(5);
+      expect(val.status).toEqual(TaskShoperRequestStatusValue.making);
       done();
     });
     shoperService.addTask(filonMerchandise);
