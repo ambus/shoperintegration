f91ecc71a08058c50bacebbb19e57ab5324211b4
Szymon Standarski <s.standarski@gmail.com>
2019-05-05 23:27:55 +0200

    feat(shoperService): dodano funkcję ustawiającą connection token do taska

--- src/shoper/shoper-service.ts
+++ src/shoper/shoper-service.ts
@@ -1,13 +1,14 @@
 import { getLogger, Logger } from "log4js";
 import { BehaviorSubject, Observable, OperatorFunction, Subject, zip } from "rxjs";
-import { delay, map, share, tap } from "rxjs/operators";
+import { delay, map, share, switchMap, tap } from "rxjs/operators";
 import { Config } from "../config/config";
 import { FilonMerchandise } from "../models/filon-merchandise";
 import { Task } from "../models/task";
 import { TaskShoperRequestStatusValue } from "../models/task-shoper-request-status-value";
+import { ShoperGetToken } from "./shoper-get-token";
 import { createTaskRequest } from "./utils/create-task-request";
-import { setStatus } from "./utils/set-status";
 import { setEndTime } from "./utils/set-end-time";
+import { setStatus } from "./utils/set-status";
 
 export class ShoperService {
   logger: Logger;
@@ -29,13 +30,27 @@ export class ShoperService {
 
   doingTask$: Observable<Task> = zip(this._taskRequest$, this.connectionPoolIsFree$).pipe(
     map(([s, f]) => s),
+    this.setConnectionToken(),
     setStatus(TaskShoperRequestStatusValue.making),
-    this.makeTask(),
-    share()
   );
 
-  makeTask(): OperatorFunction<Task, Task> {
-    return (source: Observable<Task>) => source.pipe(delay(2000));
+  setConnectionToken(): OperatorFunction<Task, Task> {
+    return (source: Observable<Task>) =>
+      source.pipe(
+        switchMap(
+          (task: Task) => ShoperGetToken.getToken(Config.getInstance().shoperConfig.userToken, false),
+          (outerValue, innerValue, outerIndex, innerIndex) => ({
+            outerValue,
+            innerValue,
+            outerIndex,
+            innerIndex
+          })
+        ),
+        map((val: { outerValue: Task; innerValue: string; outerIndex: number; innerIndex: number; }) => {
+          val.outerValue.shoperConnectionTokenID = val.innerValue;
+          return val.outerValue;
+        })
+      );
   }
 
   doneTask$: Observable<Task> = this.doingTask$.pipe(
@@ -43,8 +58,7 @@ export class ShoperService {
     setStatus(TaskShoperRequestStatusValue.done),
     setEndTime(),
     tap((request: Task) => this.logger.info(`Wykonano zadanie o id ${request.id} i czasie ${request.endTime}`)),
-    tap((request: Task) => this.connectionPoolIsFree$.next()),
-    share()
+    tap((request: Task) => this.connectionPoolIsFree$.next())
   );
 
   endTask(): OperatorFunction<Task, Task> {
