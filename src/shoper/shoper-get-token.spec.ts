bf230f5eb1e98172a53bf21084f937b1f6aab620
Szymon Standarski <s.standarski@gmail.com>
2019-05-06 09:53:49 +0200

    style(shoperGetToken): poprawiono formatowanie i import

--- src/shoper/shoper-get-token.spec.ts
+++ src/shoper/shoper-get-token.spec.ts
@@ -1,8 +1,8 @@
+import { of, throwError } from "rxjs";
+import { AjaxResponse } from "rxjs/ajax";
 import { Config } from "../config/config";
-import { ShoperGetToken } from "./shoper-get-token";
-import { ajax, AjaxResponse } from "rxjs/ajax";
-import { of } from "rxjs";
 import { stringGenerator } from "../lib/string-generator";
+import { ShoperGetToken } from "./shoper-get-token";
 
 let mockupData: AjaxResponse = {
   originalEvent: null,
@@ -33,7 +33,7 @@ describe("shoperGetToken", () => {
   //   expect(ShoperGetToken.createXHR()).toBeDefined();
   // });
 
-  it("przy drugim wywołaniu powinna zwrócić ten sam token", (done) => {
+  it("przy drugim wywołaniu powinna zwrócić ten sam token", done => {
     ShoperGetToken.getToken(Config.getInstance().shoperConfig.userToken, true).subscribe((val: string) => {
       expect(val).toBeDefined();
       mockupData.response.access_token = stringGenerator();
@@ -44,7 +44,7 @@ describe("shoperGetToken", () => {
     });
   });
 
-  it("przy drugim wywołaniu i refreshu powinna zwrócić nowy obiekt", (done) => {
+  it("przy drugim wywołaniu i refreshu powinna zwrócić nowy obiekt", done => {
     ShoperGetToken.getToken(Config.getInstance().shoperConfig.userToken, true).subscribe((val: string) => {
       expect(val).toBeDefined();
       mockupData.response.access_token = stringGenerator();
