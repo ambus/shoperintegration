d36d100b7d0ae62730513f70544730b561725c18
Szymon Standarski <s.standarski@gmail.com>
2019-05-12 06:29:57 +0200

    test(shoperStockService): dodano podstawową implementację testów

---
+++ src/shoper/shoper-stock-service/shoper-stock-service.spec.ts
@@ -0,0 +1,23 @@
+import { Config } from "../../config/config";
+import { ShoperStockService } from "./shoper-stock-service";
+import { stringGenerator } from "../../lib/string-generator";
+import { ShoperStock } from "../../models/shoper-stock";
+
+describe("shoperGetToken", () => {
+  it("musi zawierać konstruktor", done => {
+    let shoperStockService = new ShoperStockService(Config.getInstance());
+    expect(shoperStockService).toBeDefined();
+  });
+
+  it("funkcja _getAjaxConnection musi zwrócić obiekt AjaxRequest", () => {
+    let shoperStockService = new ShoperStockService(Config.getInstance());
+    expect(shoperStockService._getAjaxStock).toBeDefined();
+  });
+
+  it("funkcja getStock musi zwracać obiekt Observable", done => {
+    let shoperStockService = new ShoperStockService(Config.getInstance());
+    shoperStockService.getStock(stringGenerator(), stringGenerator()).subscribe((observer: ShoperStock) => {
+      done();
+    });
+  });
+});
