8d596eb0e25cc968719c6407b4cb59084cb24904
Szymon Standarski <s.standarski@gmail.com>
2019-05-14 19:06:10 +0200

    test(mockup): dodano mockup stringa z danymi z filona oraz obiekty FilonMerchandise

---
+++ test/mockup/filon-merchandise.mockup.ts
@@ -0,0 +1,28 @@
+import { FilonMerchandise } from "../../src/models/filon-merchandise";
+
+export const filonMerchandisesMockup: FilonMerchandise[] = [
+  {
+    product_code: "BSZK0F1FLE051",
+    stock: 2,
+    price: "139,57",
+    priceE: "15,00"
+  },
+  {
+    product_code: "BSZK0F1FLE011",
+    stock: 1,
+    price: "139,57",
+    priceE: "14,00"
+  },
+  {
+    product_code: "BSZK0IZMWAS02",
+    stock: 1,
+    price: "16,00",
+    priceE: "11,00"
+  },
+  {
+    product_code: "SBARDC22",
+    stock: 6,
+    price: "824,10",
+    priceE: "12,00"
+  }
+];
