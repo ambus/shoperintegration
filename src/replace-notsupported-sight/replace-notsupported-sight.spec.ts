6d9bddc84257758949b0bfa21858edaad33d5337
Szymon Standarski <s.standarski@gmail.com>
2019-05-21 08:58:07 +0200

    feat(replaceNotsupportedSight): dodano funkcję podmieniającą niewspierane przez shopear znaki na kody

---
+++ src/replace-notsupported-sight/replace-notsupported-sight.spec.ts
@@ -0,0 +1,21 @@
+import { of } from "rxjs";
+import { FilonMerchandise } from "../models/filon-merchandise";
+import { replaceNotSupportedSight } from "./replace-notsupported-sight";
+
+let filonMerchandisesMockup: FilonMerchandise[] = [
+  {
+    product_code: "BSZK0F1FLE051#",
+    stock: 2,
+    price: "139,57",
+    priceE: "15,00"
+  }
+];
+describe("replaceComma", () => {
+  it("w obiekcie filonMerchandise w polu price powinien zamienić przecinki na kropki", () => {
+    of(filonMerchandisesMockup)
+      .pipe(replaceNotSupportedSight())
+      .subscribe((filonMerchandises: FilonMerchandise[]) => {
+        expect(filonMerchandises[0].product_code).toBe("BSZK0F1FLE051\u0023");
+      });
+  });
+});
---
+++ src/replace-notsupported-sight/replace-notsupported-sight.ts
@@ -0,0 +1,22 @@
+import { OperatorFunction, Observable, throwError } from "rxjs";
+import { FilonMerchandise } from "../models/filon-merchandise";
+import { map, catchError } from "rxjs/operators";
+
+export function replaceNotSupportedSight(): OperatorFunction<FilonMerchandise[], FilonMerchandise[]> {
+  return (source: Observable<FilonMerchandise[]>) => {
+    return source.pipe(
+      map((filonMerchandises: FilonMerchandise[]) => {
+        filonMerchandises.map((filonMerchandise: FilonMerchandise) => {
+          filonMerchandise.product_code = filonMerchandise.product_code.replace("#", "\u0023");
+          return filonMerchandise;
+        });
+        return filonMerchandises;
+      }),
+      catchError((err: any, caught: Observable<FilonMerchandise[]>) => {
+        this.logger.error(`Napotkano błąd podczas zmieniania nieobsługiwanych znaków w danych z filona`, err);
+        throwError(err);
+        return caught;
+      })
+    );
+  };
+}
